08ae8dee97e1a925c2d85e1365e465693352c877
EricOsowski <eric.j.osowski@icloud.com>
2020-10-14 19:45:55 -0500

    Finished badge appending and readme generating

---
+++ README.md
@@ -0,0 +1,66 @@
+Project Title:
+  -------------
+
+  ## ReadMe Generator
+  ![License](https://img.shields.io/badge/License-TheUnlicense-blue)
+## Description
+--------------
+
+This is a command line based README generator, it generates a README based off of user input
+
+https://drive.google.com/file/d/1APi_XeVgvM0oNQjVhGt-5jV7I3JNWpv9/view
+
+
+## Table of Contents 
+
+* [Installation](#installation)
+-------------------------------
+* [Usage](#usage)
+-----------------
+* [Tests](#tests)
+-----------------
+* [License](#license)
+---------------------
+* [Contributing](#contributing)
+-------------------------------
+* [Questions](#questions)
+-------------------------
+
+## Installation
+
+Installation directions:
+========================
+npm install
+
+
+## Usage
+
+Usage directions
+================
+Run node index, follow prompts and a professional Readme is going to be generated in the folder that the index.js is located in.
+
+
+## Tests
+
+Commands to run tests:
+======================
+Well I don't have any tests per-say.
+
+
+## License
+----------
+
+Currently under the The Unlicense license.
+
+
+## Contributing
+
+Heres how you can contribute!
+=============================
+Email me, at my email below and I'll give you permission there.
+
+
+## Questions
+------------
+
+Questions can be directed to eric.j.osowski@icloud.com. Here is my [GitHub](https://github.com/EricJamesOsowski) for more of my work
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,81 +1,75 @@
-function renderLicenseBadge(license) {
-  if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
-  }
-  return ''
-}
-
-function renderLicenseLink(license) {
-  if (license !== "None") {
-    return (
-      `\n* [License](#license)\n`
-    )
-  }
-  return ''
-}
-
-function renderLicenseSection(license) {
-  if (license !== "None") {
-    return (
-      `## License
-
-This project is licensed under the ${license} license.`
-    )
-  }
-  return ''
-}
-
 function generateMarkdown(data) {
-  return `## ${data.title}
-${renderLicenseBadge(data.license)}
+  function deleteWhiteSpace(string){
+    return string.replace(/\s/g, '');
+  }
+  
+  const license = deleteWhiteSpace(data.license);
+  
+  return `Project Title:
+  -------------
 
+  ## ${data.title}
+  ![License](https://img.shields.io/badge/License-${license}-blue)
 ## Description
+--------------
 
 ${data.description}
+
 
 ## Table of Contents 
 
 * [Installation](#installation)
-
+-------------------------------
 * [Usage](#usage)
-${renderLicenseLink(data.license)}
-* [Contributing](#contributing)
-
+-----------------
 * [Tests](#tests)
-
+-----------------
+* [License](#license)
+---------------------
+* [Contributing](#contributing)
+-------------------------------
 * [Questions](#questions)
+-------------------------
 
 ## Installation
 
-To install necessary dependencies, run the following command:
-
-\`\`\`
+Installation directions:
+========================
 ${data.installation}
-\`\`\`
+
 
 ## Usage
 
+Usage directions
+================
 ${data.usage}
 
-${renderLicenseSection(data.license)}
-  
-## Contributing
-
-${data.contributing}
 
 ## Tests
 
-To run tests, run the following command:
-
-\`\`\`
+Commands to run tests:
+======================
 ${data.test}
-\`\`\`
+
+
+## License
+----------
+
+Currently under the ${data.license} license.
+
+
+## Contributing
+
+Heres how you can contribute!
+=============================
+${data.contributing}
+
 
 ## Questions
+------------
 
-If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
-
-`;
+Questions can be directed to ${data.email}. Here is my [GitHub](https://github.com/${data.github}) for more of my work`;
 }
+
 
 module.exports = generateMarkdown;
